8b446da2cdb02cd157894a4cbeffa1891f8a6209
root <jaeho3918@naver.com>
2020-03-03 09:46:00 -0500

    18

--- crawler.py
+++ crawler.py
@@ -49,54 +49,63 @@ last_result = {}
 
 
 def data():
-    for key, item in URLS.items():
-        html = requests.get(item, headers=headers).text
-        xpath_data = etree.HTML(html)
-        
-        for idx, xpath in enumerate(XPATHS[key]):
-            text = xpath_data.xpath(xpath)
-            if key == "CUR":
-                print(text[0].strip())
-                real_result[CUR_TABLE[idx]] = float(text[0].strip().replace(",", ""))
-            
-            elif len(key) == 2:
-                if idx == 1:
-                    real_result[f"YES{key}"] = float(text[0].strip().replace(",", ""))
+    try:
+        print(f"Crawler Start{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S utc')}")
+        for key, item in URLS.items():
+            html = requests.get(item, headers=headers).text
+            xpath_data = etree.HTML(html)
+
+            for idx, xpath in enumerate(XPATHS[key]):
+                text = xpath_data.xpath(xpath)
+                if key == "CUR":
+                    print(text[0].strip())
+                    real_result[CUR_TABLE[idx]] = float(text[0].strip().replace(",", ""))
+
+                elif len(key) == 2:
+                    if idx == 1:
+                        real_result[f"YES{key}"] = float(text[0].strip().replace(",", ""))
+                    else:
+                        real_result[key] = float(text[0].strip().replace(",", ""))
+
                 else:
+                    print(float(text[0].strip().replace(",", "")))
                     real_result[key] = float(text[0].strip().replace(",", ""))
-            
-            else:
-                print(float(text[0].strip().replace(",", "")))
-                real_result[key] = float(text[0].strip().replace(",", ""))
-    
+    except:
+        print(f"Crawler ERROR{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S utc')}")
+
     real_result["DATE"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S utc')
-    
+
     last_buf = real_result.copy()
     last_buf.pop("DATE")
     last_buf.pop("YESAU")
     last_buf.pop("YESAG")
-    
+
     last_date = datetime.datetime.now().strftime('%Y%m%d')
     last_result[last_date] = last_buf
-    
+
     print(real_result, last_result)
-    
-    cred = credentials.Certificate("./gsledger-29cad-firebase-adminsdk-o5w6i-4213914df7.json")
-    firebase_admin.initialize_app(cred, {'databaseURL': 'https://gsledger-29cad.firebaseio.com/'})
-    
+
+    try:
+        cred = credentials.Certificate("./gsledger-29cad-firebase-adminsdk-o5w6i-4213914df7.json")
+        firebase_admin.initialize_app(cred, {'databaseURL': 'https://gsledger-29cad.firebaseio.com/'})
+
+    except:
+        pass
+
+
     ref = db.reference(f"/{REALTIME_DB_PATH}")
     ref.update(real_result)
-    
+
     ref = db.reference(f"/{LASTTIME_DB_PATH}")
     ref.update(last_result)
-
+    print(f"Firebase Upload {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S utc')}")
 
 if __name__ == "__main__":
     data()
     sched = BackgroundScheduler(timezone="utc")
     sched.start()
     sched.add_job(data, 'cron', second='*/18', day_of_week='mon-fri', id="data_week")  # second='*/21'
-    # sched.add_job(data, 'cron', hour='0-7', minute='*/5', day_of_week='sat', second='18', id="data_sat")
+    sched.add_job(data, 'cron', hour='0-7', minute='*/5', second='18', day_of_week='sat', id="data_sat")
     # sched.add_job(quit_chrome_hoilday, 'cron', hour='7', minute='8', day_of_week='sat', id="holiday_quit")
     # sched.add_job(website, 'cron', day='*/1', hour='5', minute='18', id="website")
     print('scheduler start')

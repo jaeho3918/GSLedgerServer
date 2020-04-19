ecc30e9f7e0d0dff61dde3b44b420b4712e47762
ganat <jaeho3918@naver.com>
2020-04-20 00:22:44 +0900

    18

--- crawler.py
+++ crawler.py
@@ -4,9 +4,10 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-import datetime
+from datetime import datetime, timedelta
 import time
 import logging
+import random
 
 REALTIME_DB_PATH = "sYTVBn6F18VT6Ykw6L"
 LASTTIME_DB_PATH = "OGn6sgTK6umHojW6QV"
@@ -15,10 +16,12 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 
 URLS = {
-    "AU": "https://kr.investing.com/commodities/gold-historical-data",#https://kr.investing.com/currencies/xau-usd-historical-data
-    "AG": "https://kr.investing.com/commodities/silver-historical-data",#https://kr.investing.com/currencies/xag-usd-historical-data
+    # https://kr.investing.com/currencies/xau-usd-historical-data
+    "AG": "https://kr.investing.com/commodities/silver-historical-data",
+    # https://kr.investing.com/currencies/xag-usd-historical-data
     "CUR": 'https://kr.investing.com/currencies/exchange-rates-table',
     "CNY": 'https://kr.investing.com/currencies/usd-cny-historical-data',
+    "AU": "https://www.investing.com/commodities/gold-historical-data",
     "INR": 'https://kr.investing.com/currencies/usd-inr-historical-data'
 }
 
@@ -56,27 +59,31 @@ def data():
     try:
         logger.info("Crawler Start")
         for key, item in URLS.items():
+            rad = random.randint(1, 15)
+            print(key, rad)
+            time.sleep(5 + rad)
             html = requests.get(item, headers=headers).text
             xpath_data = etree.HTML(html)
-
             for idx, xpath in enumerate(XPATHS[key]):
                 text = xpath_data.xpath(xpath)
                 if key == "CUR":
                     print(text[0].strip())
                     real_result[CUR_TABLE[idx]] = float(text[0].strip().replace(",", ""))
-
+                
                 elif len(key) == 2:
                     if idx == 1:
                         real_result[f"YES{key}"] = float(text[0].strip().replace(",", ""))
                     else:
                         real_result[key] = float(text[0].strip().replace(",", ""))
-
+                
                 else:
                     print(float(text[0].strip().replace(",", "")))
                     real_result[key] = float(text[0].strip().replace(",", ""))
     except:
         logger.info("Crawler ERROR")
-
+    
+    now = datetime.utcnow()
+    
     real_result["DATE"] = datetime.datetime.now().timestamp()
     try:
         last_buf = real_result.copy()
@@ -85,26 +92,25 @@ def data():
         last_buf.pop("YESAG")
     except:
         logger.info("Crawler ERROR")
-
-
+    
     last_date = datetime.datetime.now().strftime('%Y%m%d')
     last_result[last_date] = last_buf
-
+    
     # print(real_result, last_result)
-
+    
     try:
-        cred = credentials.Certificate("./gsledger-29cad-firebase-adminsdk-o5w6i-4213914df7.json")
+        cred = credentials.Certificate("./gsledger-29cad-firebase-adminsdk-o5w6i-247692c0a9.json")
         firebase_admin.initialize_app(cred, {'databaseURL': 'https://gsledger-29cad.firebaseio.com/'})
-
+    
     except:
         pass
-
+    
     ref = db.reference(f"/{REALTIME_DB_PATH}")
     ref.update(real_result)
-
+    
     ref = db.reference(f"/{LASTTIME_DB_PATH}")
     ref.update(last_result)
-
+    
     logger.info("Crawler Upload")
 
 
@@ -113,10 +119,10 @@ if __name__ == "__main__":
     sched = BackgroundScheduler(timezone="utc")
     sched.start()
     sched.add_job(data, 'cron', minute='*/3', day_of_week='mon-fri', id="data_week")  # second='*/21'
-    sched.add_job(data, 'cron', hour='0-7', minute='*/5', second='18', day_of_week='sat', id="data_sat")
+    # sched.add_job(data, 'cron', hour='0-7', minute='*/5', second='18', day_of_week='sat', id="data_sat")
     # sched.add_job(quit_chrome_hoilday, 'cron', hour='7', minute='8', day_of_week='sat', id="holiday_quit")
     # sched.add_job(website, 'cron', day='*/1', hour='5', minute='18', id="website")
-    print('scheduler start')
-
-    while True:
-        time.sleep(29)
+    # print('scheduler start')
+    #
+    # while True:
+    #     time.sleep(29)
--- firebase_upload.py
+++ firebase_upload.py
@@ -14,26 +14,26 @@ import json
 # 운영체제에 따라 변경
 
 chrome_option = Options()
-# chrome_option.add_argument("--headless")
-# chrome_option.add_argument("--no-sandbox")
-# chrome_option.add_argument("--disable-dev-shm-usage")
-# prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'plugins': 2, 'popups': 2,
-#                                                     'geolocation': 2, 'notifications': 2,
-#                                                     'auto_select_certificate': 2, 'fullscreen': 2,
-#                                                     'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
-#                                                     'media_stream_mic': 2, 'media_stream_camera': 2,
-#                                                     'protocol_handlers': 2, 'ppapi_broker': 2,
-#                                                     'automatic_downloads': 2, 'midi_sysex': 2,
-#                                                     'push_messaging': 2, 'ssl_cert_decisions': 2,
-#                                                     'metro_switch_to_desktop': 2,
-#                                                     'protected_media_identifier': 2, 'app_banner': 2,
-#                                                     'site_engagement': 2, 'durable_storage': 2}}
-# chrome_option.add_experimental_option('prefs', prefs)
-# chrome_option.add_argument("start-maximized")
-# chrome_option.add_argument("disable-infobars")
-# chrome_option.add_argument("--disable-extensions")
+chrome_option.add_argument("--headless")
+chrome_option.add_argument("--no-sandbox")
+chrome_option.add_argument("--disable-dev-shm-usage")
+prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'plugins': 2, 'popups': 2,
+                                                    'geolocation': 2, 'notifications': 2,
+                                                    'auto_select_certificate': 2, 'fullscreen': 2,
+                                                    'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
+                                                    'media_stream_mic': 2, 'media_stream_camera': 2,
+                                                    'protocol_handlers': 2, 'ppapi_broker': 2,
+                                                    'automatic_downloads': 2, 'midi_sysex': 2,
+                                                    'push_messaging': 2, 'ssl_cert_decisions': 2,
+                                                    'metro_switch_to_desktop': 2,
+                                                    'protected_media_identifier': 2, 'app_banner': 2,
+                                                    'site_engagement': 2, 'durable_storage': 2}}
+chrome_option.add_experimental_option('prefs', prefs)
+chrome_option.add_argument("start-maximized")
+chrome_option.add_argument("disable-infobars")
+chrome_option.add_argument("--disable-extensions")
 
-driver = webdriver.Chrome(executable_path=f'./chromedriver_win72.exe', chrome_options=chrome_option)
+driver = webdriver.Chrome(executable_path=f'/home/ganatran68/chromedriver', chrome_options=chrome_option)
 
 # driver.get("https://www.naver.com")
 
@@ -63,7 +63,7 @@ driver.quit()
 
 
 def upload():
-    cred = credentials.Certificate("./gsledger-29cad-firebase-adminsdk-o5w6i-4213914df7.json")
+    cred = credentials.Certificate("./gsledger-29cad-firebase-adminsdk-o5w6i-247692c0a9.json")
     firebase_admin.initialize_app(cred, {'databaseURL': 'https://gsledger-29cad.firebaseio.com/sYTVBn2FO8VNT9Ykw90L'})
     ref = db.reference("/REAL")
     readJson = json.load('csv_data.json')

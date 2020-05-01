78d7b667ec833e0fa83edc3f8e6302b13d2dac2b
jaeho3918 <46740030+babbybear@users.noreply.github.com>
2020-05-01 20:22:38 +0900

    18

--- messageTest.py
+++ messageTest.py
@@ -7,7 +7,7 @@ import time
 
 REALTIME1_DB_PATH = "v6WqgKE6RLT6JkFuBv"
 
-topic_limit = [False, False, False, False, False, False]
+topic_limit = [False, False, False, False, False, False, False, False, False, False, False, False]
 
 cred = credentials.Certificate(
     "./gsledger-29cad-firebase-adminsdk-o5w6i-639acb814a.json")  # gsledger-29cad-firebase-adminsdk-o5w6i-4213914df7.json
@@ -15,11 +15,8 @@ cred = credentials.Certificate(
 firebase_admin.initialize_app(cred, {'databaseURL': 'https://gsledger-29cad.firebaseio.com/'})
 
 
-def message():
+def message(topic_limit):
     # See documentation on defining a message payload.
-    global topic_limit
-    global cred
-
     try:
         ref = db.reference(f"/{REALTIME1_DB_PATH}").get()
 
@@ -27,16 +24,13 @@ def message():
 
         title = f"Price Alert"
 
-        body = ""
-
         price_list = [1.0, 2.0, 3.0]
+        price_list = [0.1, 0.2, 0.3]
 
         AU = (ref['AU'] - ref['YESAU']) / ref['YESAU'] * 100
         AG = (ref['AG'] - ref['YESAG']) / ref['YESAG'] * 100
 
         topic_list = ["Alpha", "Beta", "Gamma"]
-
-        # title = f"Price Alert : {ref['DATE'][:16]} UTC"
 
         for idx, price in enumerate(price_list):
 
@@ -48,19 +42,19 @@ def message():
                     topic_limit[idx] = True
 
             elif AU <= -1 * price:
-                if not topic_limit[idx]:
+                if not topic_limit[idx + int(len(topic_limit) * 1 / 4)]:
                     body_Slot['AU'] = [ref['AU'], F"▼ ({str(AU)[:5]}%)"]
-                    topic_limit[idx] = True
+                    topic_limit[idx + int(len(topic_limit) * 1 / 4)] = True
 
             if AG >= price:
-                if not topic_limit[idx + int(len(topic_limit) / 2)]:
+                if not topic_limit[idx + int(len(topic_limit) * 2 / 4)]:
                     body_Slot['AG'] = [ref['AG'], F"▲ (+{str(AG)[:5]}%)"]
-                    topic_limit[idx + int(len(topic_limit) / 2)] = True
+                    topic_limit[idx + int(len(topic_limit) * 2 / 4)] = True
 
             elif AG <= -1 * price:
-                if not topic_limit[idx + int(len(topic_limit) / 2)]:
+                if not topic_limit[idx + int(len(topic_limit) * 3 / 4)]:
                     body_Slot['AG'] = [ref['AG'], F"▼ ({str(AG)[:5]}%)"]
-                    topic_limit[idx + int(len(topic_limit) / 2)] = True
+                    topic_limit[idx + int(len(topic_limit) * 3 / 4)] = True
 
             body_string = ""
             gold_buf = ""
@@ -84,7 +78,7 @@ def message():
                         priority='normal',
 
                         notification=messaging.AndroidNotification(
-                            title='Price Alert',
+                            title=title,
                             body=body_string,
                             icon='',
                             color='#fd6166',
@@ -100,5 +94,9 @@ def message():
                 response = messaging.send(message)
                 # Response is a message ID string.
                 print('Successfully sent message:', topic_list[idx])
+                print('global topic_limit:', topic_limit)
     except:
         print("message error")
+
+
+message(topic_limit)

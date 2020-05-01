35b89f3af23549fef98cb524af519ea7206c06ce
jaeho3918 <46740030+babbybear@users.noreply.github.com>
2020-05-01 20:30:57 +0900

    18

--- crawler.py
+++ crawler.py
@@ -456,14 +456,14 @@ def messageLimit():
 if __name__ == "__main__":
     # data()
     sched = BackgroundScheduler(timezone="UTC")
-    sched.add_job(data, 'cron', minute='*/11', hour='0-21', day_of_week='mon-fri', id="day")
+    sched.add_job(data, 'cron', minute='*/11', hour='0-20', day_of_week='mon-fri', id="day")
     sched.add_job(data, 'cron', minute='*/11', hour='22-23', day_of_week='mon-fri', id="dayNight")
-    sched.add_job(messageLimit, 'cron', minute='*/11', hour='22-23', day_of_week='mon-fri', id="reset_message_limit")
+    sched.add_job(messageLimit, 'cron', minute='56', hour='21', day_of_week='mon-fri', id="reset_message_limit")
     # sched.add_job(data, 'cron', minute='*/11', day_of_week='sun', id="sunday")
     sched.add_job(data, 'cron', minute='*/11', hour='22-23', day_of_week='sun', id="sunday")
 
-    sched.add_job(getShortChartBuf, 'cron', minute='50', hour='23', day_of_week='mon-fri', id="shortChart")
-    sched.add_job(getLongChartBuf, 'cron', minute='50', hour='23', day_of_week='sat', id="longChart")
+    sched.add_job(getShortChartBuf, 'cron', minute='56', hour='21', day_of_week='mon-fri', id="shortChart")
+    sched.add_job(getLongChartBuf, 'cron', minute='56', hour='21', day_of_week='sat', id="longChart")
 
     # sched.add_job(data, 'cron', minute='0-59/11', hour='22-23', day_of_week='sun-fri', id="night")
     # sched.add_job(data, 'cron', hour='0-7', minute='*/5', second='18', day_of_week='sat', id="data_sat")
